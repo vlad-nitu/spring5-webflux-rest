eec61cfc98dee24f9dd79bf76c050377aaebb7e0
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-04 15:55:29 +0300

    Fixed MongoDB embedded bug
    More info here: https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.6-Release-Notes\#embedded-mongo and problem solved on SO:https://stackoverflow.com/questions/70047380/enableautoconfigurationexclude-on-tests-failed-in-spring-boot-2-6-0

--- src/test/java/vlad/springframework/spring5webfluxrest/ApplicationTests.java
+++ src/test/java/vlad/springframework/spring5webfluxrest/ApplicationTests.java
@@ -2,7 +2,9 @@ package vlad.springframework.spring5webfluxrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 @SpringBootTest
 class ApplicationTests {
 
