109a00e90baf878f2becaa76980e7d77597adac3
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-04 15:13:25 +0300

    Intial commit

---
+++ src/main/java/vlad/springframework/spring5webfluxrest/Application.java
@@ -0,0 +1,13 @@
+package vlad.springframework.spring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/vlad/springframework/spring5webfluxrest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.springframework.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
