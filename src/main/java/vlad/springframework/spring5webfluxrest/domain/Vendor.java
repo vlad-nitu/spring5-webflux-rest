c7d2a218df7cd351b1c4b580f7484417df55a448
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-04 18:25:32 +0300

    Removed commented LoC in Vendor class
    while also upgraded my GPG Key to allow for both university and personal email address. Useful links: https://stephenreescarter.net/signing-git-commits-with-a-keybase-gpg-key/ for Signing Git Commits & https://stackoverflow.com/questions/67380948/could-not-make-my-second-mails-commits-as-verified-on-github for updating GPG Public Key
    
    Took 8 minutes

--- src/main/java/vlad/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/vlad/springframework/spring5webfluxrest/domain/Vendor.java
@@ -15,28 +15,4 @@ public class Vendor {
     private String id;
     private String firstName;
     private String lastName;
-
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
 }
